76f181e38f3c8012071e17bb48b3f8a2cbcf0ec4
HHUUYYLLEE <lebahuybk@gmail.com>
2024-12-06 16:53:48 +0700

    module

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import fastifyMultipart from '@fastify/multipart';
-
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+const fastifyMultipart = require('@fastify/multipart');
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({ bodyLimit: 1073741824 }));
   await app.register(fastifyMultipart);
   const configService = app.get(ConfigService);
   app.enableCors({
-    origin: [configService.get<string>('frontendVercel') as string, /https*:\/\/localhost.*$/, /^https*:\/\/192\.168\..*$/]
+    origin: [
+      configService.get<string>('frontendVercel') as string,
+      /https*:\/\/localhost.*$/,
+      /^https*:\/\/192\.168\..*$/,
+      /http*:\/\/localhost.*$/,
+      /^http*:\/\/192\.168\..*$/
+    ]
   });
 
   const port = configService.get('port');
